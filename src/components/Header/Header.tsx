5462637723c3260ea969be35a1fc25e42c33cfc8
Guilherme Vieira <guivpw68@gmail.com>
2021-11-16 16:39:10 -0300

    fix(emotion): :lipstick: fix layout alignment to avoid no overflow

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 import LogoutIcon from '@mui/icons-material/PowerSettingsNew'
-import { AppBar, IconButton, Stack } from '@mui/material'
+import { AppBar, IconButton, Stack, Toolbar } from '@mui/material'
 
 import { Container, Content, Title, RightContent } from './Header.styled'
 
@@ -11,23 +11,25 @@ export interface HeaderProps {
 }
 
 export const Header = ({ handleLogout, authenticated = false }: HeaderProps) => (
-	<AppBar color="transparent" variant="elevation">
-		<Container>
-			<Content>
-				<Link href="/" passHref>
-					<Title variant="h1">Up Meal</Title>
-				</Link>
-			</Content>
-			{authenticated && (
-				<RightContent>
-					<Stack direction="row" alignItems="center" justifyContent="flex-end">
-						<IconButton onClick={handleLogout}>
-							<LogoutIcon />
-						</IconButton>
-					</Stack>
-				</RightContent>
-			)}
-		</Container>
+	<AppBar color="transparent" variant="elevation" position="static">
+		<Toolbar>
+			<Container>
+				<Content>
+					<Link href="/" passHref>
+						<Title variant="h1">Up Meal</Title>
+					</Link>
+				</Content>
+				{authenticated && (
+					<RightContent>
+						<Stack direction="row" alignItems="center" justifyContent="flex-end">
+							<IconButton onClick={handleLogout}>
+								<LogoutIcon />
+							</IconButton>
+						</Stack>
+					</RightContent>
+				)}
+			</Container>
+		</Toolbar>
 	</AppBar>
 )
 

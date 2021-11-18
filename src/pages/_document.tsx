012db254cb4e166d7acf306c43253aead6bbf3e8
Guilherme Vieira <guivpw68@gmail.com>
2021-11-18 00:34:10 -0300

    refactor: abstract descriptions

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,11 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
+					<link rel="icon" href="/favicon.ico" />
+					<meta
+						name="description"
+						content="Evite o desperdício através de uma plataforma de busca de alimentos prestes a vencer"
+					/>
 				</Head>
 				<body>
 					<Main />

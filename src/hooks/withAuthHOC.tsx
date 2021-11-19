02c0ee737460274e001d0e7f2917c03addff7790
Guilherme Vieira <guivpw68@gmail.com>
2021-11-19 19:25:28 -0300

    fix: with auth HOC

--- src/hooks/withAuthHOC.tsx
+++ src/hooks/withAuthHOC.tsx
@@ -4,15 +4,16 @@ import { useRouter } from 'next/router'
 
 import { JSX } from '@emotion/react/jsx-runtime'
 
+import { useStoreon } from './useStoreon'
+
 export const withAuth = (WrappedComponent: any) => {
 	// eslint-disable-next-line react/display-name
 	return (props: JSX.IntrinsicAttributes) => {
 		if (typeof window !== 'undefined') {
 			const Router = useRouter()
+			const { token } = useStoreon('token')
 
-			const accessToken = localStorage.getItem('token')
-
-			if (!accessToken) {
+			if (!token) {
 				Router.replace('/')
 				return null
 			}

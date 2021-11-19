c1eefeaaf3935c08ffd5937643b5dfe937d15f5a
Guilherme Vieira <guivpw68@gmail.com>
2021-11-19 19:25:42 -0300

    fix: get storeon token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import { Axios } from 'axios'
 
+import { Shop } from './entities'
+
 export const api = new Axios({
 	baseURL: process.env.API_URL || 'http://localhost:8080/api/rest',
 	timeout: 5000,
@@ -28,9 +30,11 @@ export const api = new Axios({
 api.interceptors.request.use(config => {
 	const { headers } = config
 
-	const token = localStorage.getItem('token')
+	const storeon = localStorage.getItem('storeon')
 
-	if (token) {
+	if (storeon) {
+		const { token } = JSON.parse(storeon) as { token: string; shop: Shop }
+
 		const newHeaders = { ...headers, Authorization: `Basic ${token}` }
 
 		config.headers = newHeaders

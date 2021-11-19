b3eb6307ea60982fa6bda4f4d9cc5eb8649ebfa0
Guilherme Vieira <guivpw68@gmail.com>
2021-11-19 19:25:03 -0300

    feat: add min date and textfield labels

--- src/components/FoodModal/FoodModal.tsx
+++ src/components/FoodModal/FoodModal.tsx
@@ -62,76 +62,83 @@ export const FoodModal = ({
 			okLoading={loading}
 			onOkDisabled={loading || !form?.name || !form?.quantity || !form?.validationDate}
 			onOk={() => handleOk(form as Food)}
+			dialogProps={{ fullWidth: true }}
 			showCloseButton
+			title={data ? 'Alterar alimento' : 'Adicionar alimento'}
 		>
-			<Grid container justifyContent="center" spacing={2}>
-				<Grid item xs={12} md>
-					<Autocomplete
-						options={foodOptions}
-						freeSolo
-						autoHighlight
-						value={data?.name}
-						fullWidth
-						onChange={(_, newValue) => handleChangeName(newValue ?? '')}
-						renderInput={params => (
-							<TextField required variant="outlined" {...params} helperText="Nome*" />
-						)}
-					/>
-				</Grid>
-				<Grid item xs={12} md>
-					<FormControl required component="fieldset">
-						<FormLabel component="legend">Disponibilidade</FormLabel>
-						<RadioGroup
-							row
-							value={data?.isAvailable}
-							onChange={e => handleChangeAvailability(e.target.value)}
-						>
-							<FormControlLabel value={false} control={<Radio />} label="Sim" />
-							<FormControlLabel value={true} control={<Radio />} label="Não" />
-						</RadioGroup>
-					</FormControl>
-				</Grid>
-			</Grid>
-			<Grid container justifyContent="center" spacing={2} sx={{ marginTop: '24px' }}>
-				<Grid item xs={12} md>
-					<TextField
-						helperText="Quantidade*"
-						variant="outlined"
-						required
-						onChange={e => handleChangeQuantity(parseFloat(e.target.value))}
-						value={data?.quantity}
-						type="number"
-						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-						// @ts-ignore
-						step=".01"
-						onInput={(e: any) => {
-							e.target.value = parseFloat(e.target.value.toString().slice(0, 4))
-						}}
-						InputProps={{
-							inputProps: {
-								min: 0,
-								max: 100
-							},
-							endAdornment: (
-								<InputAdornment position="end">
-									<strong>kg</strong>
-								</InputAdornment>
-							)
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12} md>
-					<LocalizationProvider dateAdapter={AdapterDateFns}>
-						<DatePicker
-							value={data?.validationDate}
-							onChange={newValue => {
-								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-								// @ts-ignore
-								handleChangeValidate(newValue ?? '')
-							}}
-							renderInput={params => <TextField {...params} />}
+			<Grid container sx={{ marginTop: '20px' }}>
+				<Grid container item justifyContent="center" spacing={2}>
+					<Grid item xs={12} md>
+						<Autocomplete
+							options={foodOptions}
+							freeSolo
+							autoHighlight
+							value={data?.name}
+							fullWidth
+							onChange={(_, newValue) => handleChangeName(newValue ?? '')}
+							renderInput={params => (
+								<TextField required variant="outlined" {...params} label="Nome" />
+							)}
 						/>
-					</LocalizationProvider>
+					</Grid>
+					<Grid item xs={12} md>
+						<FormControl required component="fieldset">
+							<FormLabel component="legend">Disponibilidade</FormLabel>
+							<RadioGroup
+								row
+								value={data?.isAvailable}
+								onChange={e => handleChangeAvailability(e.target.value)}
+							>
+								<FormControlLabel value={false} control={<Radio />} label="Sim" />
+								<FormControlLabel value={true} control={<Radio />} label="Não" />
+							</RadioGroup>
+						</FormControl>
+					</Grid>
+				</Grid>
+				<Grid container justifyContent="center" spacing={2} sx={{ marginTop: '12px' }}>
+					<Grid item xs={12} md>
+						<TextField
+							label="Quantidade*"
+							variant="outlined"
+							required
+							fullWidth
+							onChange={e => handleChangeQuantity(parseFloat(e.target.value))}
+							value={data?.quantity}
+							type="number"
+							// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+							// @ts-ignore
+							step=".01"
+							onInput={(e: any) => {
+								e.target.value = parseFloat(e.target.value.toString().slice(0, 4))
+							}}
+							InputProps={{
+								inputProps: {
+									min: 0,
+									max: 100
+								},
+								endAdornment: (
+									<InputAdornment position="end">
+										<strong>kg</strong>
+									</InputAdornment>
+								)
+							}}
+						/>
+					</Grid>
+					<Grid item xs={12} md>
+						<LocalizationProvider dateAdapter={AdapterDateFns}>
+							<DatePicker
+								label="Data de validade"
+								value={data?.validationDate}
+								minDate={new Date()}
+								onChange={newValue => {
+									// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+									// @ts-ignore
+									handleChangeValidate(newValue ?? '')
+								}}
+								renderInput={params => <TextField required {...params} />}
+							/>
+						</LocalizationProvider>
+					</Grid>
 				</Grid>
 			</Grid>
 		</Modal>

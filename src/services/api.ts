7988561520fe2234969a6690d7a32a984e4c4a5d
Guilherme Vieira <guivpw68@gmail.com>
2021-11-19 17:50:44 -0300

    feat: add client request and response transformers

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,28 @@
 import { Axios } from 'axios'
 
-const { API_URL } = process.env
-
 export const api = new Axios({
-	baseURL: API_URL || '127.0.0.1:8080/api/rest',
-	timeout: 5000
+	baseURL: process.env.API_URL || 'http://localhost:8080/api/rest',
+	timeout: 5000,
+	headers: {
+		'Content-type': 'application/json'
+	},
+	validateStatus: status => status >= 200 && status < 400,
+	transformRequest: [
+		function transformRequest(data) {
+			if (data && JSON.stringify(data)) {
+				const formattedData = JSON.stringify(data)
+
+				return formattedData
+			}
+
+			return data
+		}
+	],
+	transformResponse: [
+		function transformResponse(data) {
+			return JSON.parse(data)
+		}
+	]
 })
 
 api.interceptors.request.use(config => {
